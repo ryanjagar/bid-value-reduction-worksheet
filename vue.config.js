e4bb94266a91be196432020579c8c22611935f94
ryanjagar <ryan.agar@gov.yk.ca>
2021-11-06 08:06:10 -0700

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  publicPath: '/bid-value-reduction/',
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: `
-          @import "@/scss/_variables.scss";
-        `
-      }
-    }
-  }
+  publicPath: '/bid-value-reduction/'
 };
