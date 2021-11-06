b53f0a91d46da6f29e81d94c8b330ddbc894ac00
ryanjagar <ryan.agar@gov.yk.ca>
2021-11-06 16:13:29 -0700

    two way binding on ownership

--- src/components/fnBVR/fnBVROwnership.vue
+++ src/components/fnBVR/fnBVROwnership.vue
@@ -1,57 +1,73 @@
+
 <template>
   <v-card>
     <v-card-title>Ownership</v-card-title>
     <v-card-text>Enter the dollar value of work based on the percentage of Yukon First Nations ownership for the business or subcontracted business(es). This must exclude the amount for Yukon First Nations labour.</v-card-text>
      <v-card-text>
+       <br /> 
       <v-row>
         <v-col>
           <v-text-field 
-            v-model="ownership.businessName"
-            label= "Business Name"> 
+            label= "Business Name"
+            v-model="ownership.businessName"> 
           </v-text-field>
         </v-col>
         <v-col>
           <v-text-field 
-            v-model="ownership.percentage"
-            label= "First Nation Ownership (in %)"> 
+            label= "First Nation Ownership (in %)"
+            v-model="ownership.percentage">
           </v-text-field>
         </v-col>
         <v-col>
           <v-text-field 
-            v-model="ownership.value"
-            label= "Estimated Value"> 
+            label= "Estimated Value"
+            v-model="ownership.value">
           </v-text-field>
         </v-col>
         <v-col>
           <v-text-field 
-            v-model= "bvrPercentage"
-            label= "Bid Value Reduction"> 
+            v-model="bvrPercentage"
+            label= "Bid Value Reduction">
           </v-text-field>
         </v-col>
-      
-        
       </v-row>
     </v-card-text>
   </v-card>
-
 </template>
 
 <script>
-
-
 export default {
   name: "fnBVROwnership",
   props:{
-
+    ownership: 
+    {
+      type: Object,
+      default () {
+        return {
+          businessName: "Default",
+          percentage: ""
+        }
+      }
+    }
+      
   },
   data: () => ({
-    ownership: {"businessName":"", "percentage": "", "value": "", "bvr": 0},
     
   }),
   methods: {
     
   },
+  watch: {
+    bvrPercentage: function() {
+      console.log('waa')
+      this.ownership.bvr = this.bvrPercentage
+    }
+  },
+  created () {
+  
+  },
   computed: {
+    
     cleanedValue: function () {
       return Number(this.ownership.value.replaceAll(",", ""))
     },
--- src/components/fnBidReductionCalculator.vue
+++ src/components/fnBidReductionCalculator.vue
@@ -3,9 +3,18 @@
   <v-card-title class="text-h4" >Bid Reduction Worksheet</v-card-title>
   <v-card-text>
   
-  <fnBVROwnership />
-  <fnBVRLocation />
-  <fnBVRLabour />
+  
+
+<fnBVROwnership 
+   :ownership.sync='owners[0]'
+   @update:BVR="console.log('boy howdy')"
+  />
+
+{{owners[0]}}
+  <fnBVRLocation
+  v-show="false" />
+  <fnBVRLabour
+  v-show="false" />
   </v-card-text>
 </v-card>
 
@@ -31,11 +40,13 @@ export default {
     // if owner is >= 50 then BVR = 5%
     // if owner is >= 75 then BVR = 10%
     // if owner is >= 100 then BVR = 15%
-    owners: [{"businesName":"", "fnOwnership": "", "value": "", "bvr": "" }],
+    owners: [{"businessName":"AAAA", "percentage": "0", "value": "25", "bvr": "" }],
     locations: [],
+    jack: {"businessName":"AAAA", "percentage": "", "value": "", "bvr": "" },
     labour: {"value": 0, "bvr": 0.05 },
     ownerInstance: {},
-    locationInstance: {} 
+    locationInstance: {},
+    businessName: "aName" 
     // Location formula
     // If outside Whitehorse and on title
     // BVR += 5% 
@@ -46,6 +57,12 @@ export default {
  
   },
   methods: {
+    logIT: function(text) {
+      console.log(text)
+      console.log(`jack before: ${this.jack}`)
+      this.jack = text;
+      console.log(`jack after: ${this.jack}`)
+    }
   },
   computed: {
 
