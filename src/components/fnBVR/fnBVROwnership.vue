064e56fb51fe192eb9400a8bceb6eee30185177b
ryanjagar <ryan.agar@gov.yk.ca>
2021-11-08 08:11:27 -0700

    make responsive

--- src/components/fnBVR/fnBVROwnership.vue
+++ src/components/fnBVR/fnBVROwnership.vue
@@ -1,38 +1,59 @@
 
 <template>
-  <v-card>
+<v-container>
+  
+ 
+  
     <v-card-title>Ownership</v-card-title>
     <v-card-text>Enter the dollar value of work based on the percentage of Yukon First Nations ownership for the business or subcontracted business(es). This must exclude the amount for Yukon First Nations labour.</v-card-text>
      <v-card-text>
-       <br /> 
+      
+       {{$vuetify.breakpoint.name}}
+      
       <v-row>
-        <v-col>
-          <v-text-field 
-            label= "Business Name"
-            v-model="ownership.businessName"> 
-          </v-text-field>
-        </v-col>
-        <v-col>
-          <v-text-field 
-            label= "First Nation Ownership (in %)"
-            v-model="ownership.percentage">
-          </v-text-field>
-        </v-col>
-        <v-col>
+          <v-col
+          cols=12
+          md=3
+          sm=3
+          >
+            <v-text-field 
+              label= "Business Name"
+              v-model="ownership.businessName"> 
+            </v-text-field>
+          </v-col>
+          <v-col
+          cols=12
+          md=3
+          sm=3>
+              <v-text-field 
+                label= "First Nation Ownership (in %)"
+                v-model="ownership.percentage">
+              </v-text-field>
+            </v-col>
+    
+      
+        <v-col
+        cols=12
+          md=3
+          sm=3>
           <v-text-field 
             label= "Estimated Value"
             v-model="ownership.value">
           </v-text-field>
         </v-col>
-        <v-col>
+        <v-col
+          cols=12
+          md=3
+          sm=3>
           <v-text-field 
             v-model="bvrPercentage"
             label= "Bid Value Reduction">
           </v-text-field>
         </v-col>
       </v-row>
+     
     </v-card-text>
-  </v-card>
+</v-container>
 </template>
 
 <script>
@@ -75,7 +96,19 @@ export default {
       }
       else 
         return 0
-      }  
-  }
+      },
+       itemsPerRow: function () {
+        switch (this.$vuetify.breakpoint.name) {
+          case 'xs': return 1
+          case 'sm': return 2
+          case 'md': return 3
+          case 'lg': return 4
+          case 'xl': return 6
+          
+        }
+        return null
+      } 
+  },
+  
 };
 </script>
